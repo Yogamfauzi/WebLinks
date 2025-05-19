06c93d2530c02f450832a43c11e2acaf719ec2a5
Yoga Muhammad Fauzi <yogamfauzi@gmail.com>
2025-05-20 02:23:01 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Home msg="Mie Bakar Mozaki" />
+</template>
+
+<script setup>
+  import Home from './components/home.vue';
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.flex {
+    display: flex; /* Enable flexbox */
+    align-items: center; /* Vertically center items */
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css' // Import file CSS utama
+
+createApp(App).mount('#app')
